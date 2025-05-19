2293ae815fefee911ced4dd7bca049f4b4daeeb2
Jucalino <joseluis.bmjnet@gmail.com>
2025-05-19 09:58:55 -0300

    usando pipes, parâmetros e pipes aninhados

---
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.scss']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro:any = {
+    titulo: 'Jucas onfire na programação',
+    rating: '4.98321',
+    numeroPaginas: 233,
+    preco: 44.99,
+    dataLancamento: new Date(2025, 5, 19),
+    url: 'https://github.com/Jucalino'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
